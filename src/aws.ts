05bf7baaef45f29762d5d85ad5924525f65e43fc
Chen Guo <c82guo@uwaterloo.ca>
2024-02-09 23:32:44 -0500

    describe and scan table

---
+++ src/aws.ts
@@ -0,0 +1,74 @@
+import { unmarshall } from '@aws-sdk/util-dynamodb';
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+// 加载 .env 文件中定义的环境变量到 process.env，这使得在代码中可以方便地使用这些变量。
+// 通常，.env 文件用于存储敏感信息和配置选项，如数据库密码、API 密钥等，因此它不应该被添加到版本控制系统（如 Git）中。
+// process.env 是 Node.js 中的一个全局对象，用于存储关于系统环境的信息。
+//  它是 process 对象的一个属性，其中包含了环境变量的键值对。
+
+AWS.config.update({ region: process.env.AWS_REGION });
+// 使用 AWS SDK 的 config.update 方法来配置 AWS 的默认区域。
+// process.env.AWS_REGION 是一个环境变量，表示 AWS 的区域（例如，us-west-2、eu-central-1 等）。
+// 这个值应该在.env 文件中被定义，如 AWS_REGION = us - west - 2
+
+const { DynamoDB } = AWS;
+
+const dynamodb = new DynamoDB();
+
+// 1 - Describe a table
+export const dynamodbDescribeTable = async (tableName: string) => {
+  try {
+    const res = await dynamodb
+      .describeTable({ TableName: tableName }) 
+      .promise();
+    console.log('Table retrieved', res);
+    return res;
+  } catch (e) {
+    if (e instanceof Error) {
+      return e;
+    }
+    console.error(e);
+    throw new Error('dynamodbDescribeTable error');
+  }
+};
+
+// 2 - Scan method
+export const dynamodbScanTable = async function* (
+  // 这里的function*是指该函数为generator，允许函数按需生成一系列值，而不是一次性返回一个单一值。
+  // 因此需要搭配yield实现 
+  tableName: string,
+  limit: number = 25,
+  lastEvaluatedKey?: AWS.DynamoDB.Key
+) {
+  while (true) {
+    const params: AWS.DynamoDB.ScanInput = {
+      TableName: tableName,
+      Limit: limit,
+    };
+
+    if (lastEvaluatedKey) {
+      params.ExclusiveStartKey = lastEvaluatedKey;
+    }
+
+    try {
+      const result = await dynamodb.scan(params).promise();
+      if (!result.Count) {
+        return;
+      }
+
+      lastEvaluatedKey = (result as AWS.DynamoDB.ScanOutput)
+        .LastEvaluatedKey;
+
+      result.Items = result.Items?.map((item) => unmarshall(item));
+
+      yield result;
+    } catch (e) {
+      if (e instanceof Error) {
+        throw e;
+      }
+      throw new Error('dynamodbScanTable error');
+    }
+  }
+};
